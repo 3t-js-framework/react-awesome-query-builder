7d899f3b211dc7601f47d927cece0e2ef2d5af31
E047464 <PW91Xp5MYmzdGTjqHiQp>
2020-01-16 10:12:03 +0700

    add valueSrc to querybuilderformat

--- modules/utils/queryBuilderFormat.js
+++ modules/utils/queryBuilderFormat.js
@@ -123,18 +123,13 @@ export const queryBuilderFormat = (item, config, rootQuery = null) => {
         valueType = valueType.toArray();
         let values = [];
         for (let i = 0 ; i < value.length ; i++) {
-            let val = {};
-            if(valueSrc[i] == 'field'){
-                val = {
-                    type: valueSrc[i],
-                    value: value[i],
-                };
-            } else {
-                val = {
-                    type: valueType[i],
-                    value: value[i],
-                };
-            }
+            
+            const valueSrcParse = valueSrc[i];
+            let val = {
+                type: valueType[i],
+                value: value[i],
+                valueSrc: valueSrcParse
+            };
             values.push(val);
             if (valueSrc[i] == 'field') {
                 let secondField = value[i];
